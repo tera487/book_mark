c047348ac3c137d23e6a7047a986b0bbae316f11
tera <tera.1999.kagayaku@ezweb.ne.jp>
2022-05-29 12:30:22 +0900

    add:初期設定
    react spaの基本的機能の実施

---
+++ book_mark/resources/js/app.js
@@ -0,0 +1,38 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes React and other helpers. It's a great starting point while
+ * building robust, powerful web applications using React + Laravel.
+ */
+
+require('./bootstrap');
+
+/**
+ * Next, we will create a fresh React component instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+// require('./components/Example');
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter,  Routes, Route } from "react-router-dom";
+// import Example from './components/Example';
+import About from "./About";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/about" element={<About />}/>
+    </Routes>
+    </BrowserRouter>
+  );
+};
+
+if (document.getElementById("app")) {
+  ReactDOM.render(<App />, document.getElementById("app"));
+  }
+  export default App;
