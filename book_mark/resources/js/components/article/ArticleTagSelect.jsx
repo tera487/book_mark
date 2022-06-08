e5a4e132c5ab8a7ab37b8e800552c1ac50b7c570
tera <tera.1999.kagayaku@ezweb.ne.jp>
2022-06-08 12:36:15 +0900

    clean 必要ない定数の削除

--- book_mark/resources/js/components/article/ArticleTagSelect.jsx
+++ book_mark/resources/js/components/article/ArticleTagSelect.jsx
@@ -29,18 +29,6 @@ function getStyles(name, personName, theme) {
     };
 }
 
-const names = [
-    "Oliver Hansen",
-    "Van Henry",
-    "April Tucker",
-    "Ralph Hubbard",
-    "Omar Alexander",
-    "Carlos Abbott",
-    "Miriam Wagner",
-    "Bradley Wilkerson",
-    "Virginia Andrews",
-    "Kelly Snyder",
-];
 
 export default function ArticleTagSelect(props) {
     const theme = useTheme();
